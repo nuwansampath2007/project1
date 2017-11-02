5537d2565ffe6061a3eff3e2426a15843a47566e
nuwansampath2007 <31372527+nuwansampath2007@users.noreply.github.com>
2017-11-02 13:03:55 +0530

    push

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent{
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    products: any[] = [
+        {
+            "productId":   2,
+            "productName": "Car",
+            "productCode": "TTP-112",
+            "releaseDate": "03/25/2017",
+            "description": "2000CC petrol 16 Valve Engine",
+            "price": 4200,
+            "starRating": 4.5,
+            "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Toyota_Allion_T260.jpg/1280px-Toyota_Allion_T260.jpg" 
+        },
+        {
+            "productId":   3,
+            "productName": "Bus",
+            "productCode": "LLW-1256",
+            "releaseDate": "08/15/2016",
+            "description": "3500CC diesel 24 Valve Engine",
+            "price": 54000,
+            "starRating": 4,
+            "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/f/f2/2001-2007_Toyota_Coaster_bus_01.jpg" 
+        }
+    ];
+}
