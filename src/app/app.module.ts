0a3cf493d09c7a8338fac4ae6e22e287cea341ed
nuwansampath2007 <31372527+nuwansampath2007@users.noreply.github.com>
2017-11-08 13:25:54 +0530

    8th commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,35 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo:'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo:'welcome', pathMatch: 'full'}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }   
